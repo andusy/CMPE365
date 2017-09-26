1692dd4e0984a33f61b6032dfd86433419734433
andusy <andus401@gmail.com>
2017-09-26 17:10:43 -0400

    lab 2

---
+++ Lab2/lab2.py
@@ -0,0 +1,33 @@
+# Collatz Conjecture
+def collatz(n):
+	# Hash table for intermediate values 
+	intermediate = [None] * 10000 # Fill the hash table with nulls
+	file = open("intermediate.txt", "w")
+
+	for i in range(n,0,-1): # Go from integer n to 1
+		x = i; # The current intermediate value
+		iteration = str("Iteration " + str(x) + ":")
+
+		print(iteration, end = "") # Print the iteration number
+
+		# Run the loop while the intermediate value does not equal 1 
+		#(Terminates when it reaches 1) or the value has been previously
+		# hashed into the intermediate list
+		# Don't need to test for 1 because the first run of the function will
+		# yield a 1 at the end
+		while (x != 1) and (intermediate[x] is None): 
+			intermediate[x] = x # Hash x into the xth element of the hash table
+			print (intermediate[x], end = " ") # Print the values
+			iteration = str(iteration + str(intermediate[x]) + " ") # Concatenate the string with the value
+
+			if (x % 2 == 0): # If x is even
+				x = int(x / 2) # Set the next term
+			else:
+				x = int(x * 3 + 1) # Set the next term
+
+		iteration = str(iteration + "\n") # Add a new line to the string
+		print() # Print new line to console
+		file.write(iteration) # Write the numbers generated in the iteration to a text file
+	file.close()
+
+collatz(100)
